1d39615119b2a0a160f7ad1bd4122a850a4642c6
Akash Desai <132215036+AakashDesai3105@users.noreply.github.com>
2023-12-05 18:23:35 +0530

    Changed Project Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,14 +127,14 @@ const sr = ScrollReveal({
   distance:'60px',
   duration:2500,
   delay:400,
-  reset:true,
+  reset:false ,
 })
 
 sr.reveal(`.home__data,.home__social,.contact-info`);
-sr.reveal(`.projects_content`,{origin: 'bottom',});
-sr.reveal(`.home__image,.contact-form`,{origin: 'bottom',});
-sr.reveal(`.about__image,.skills__content:nth-child(2)`,{origin: 'right'});
 
-sr.reveal(`.about__data,.skills__content:nth-child(1)`,{origin: 'left'});
+sr.reveal(`.home__image,.contact-form`,{origin: 'bottom',});
+sr.reveal(`.about__image,.skills__content:nth-child(2),.mobile,.project__description`,{origin: 'right'});
+
+sr.reveal(`.about__data,.skills__content:nth-child(1),.desktop`,{origin: 'left'});
 
 sr.reveal(`.about__data`,{origin: 'left'});
